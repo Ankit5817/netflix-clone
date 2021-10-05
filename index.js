0dae968034ff24e4bad8b2b2f8a65fb1a4c39f58
Ankit Sharma <sharma.ankit.078@gmail.com>
2021-10-05 16:37:12 +0530

    weather api

---
+++ index.js
@@ -0,0 +1,93 @@
+
+
+
+function display1(){
+    let show = document.getElementById('b1');
+    if(show.style.display == 'none'){
+        show.style.display = 'block';
+        b2.style.display='none';
+        b3.style.display='none';
+        b4.style.display='none';
+        b5.style.display='none';
+        b6.style.display='none';
+    }
+    else{
+        show.style.display = 'none';
+    }
+}
+
+function display2(){
+    let show = document.getElementById('b2');
+    if(show.style.display == 'none'){
+        show.style.display = 'block';
+        b1.style.display='none';
+        b3.style.display='none';
+        b4.style.display='none';
+        b5.style.display='none';
+        b6.style.display='none';
+    }
+    else{
+        show.style.display = 'none';
+    }
+}
+
+function display3(){
+    let show = document.getElementById('b3');
+    if(show.style.display == 'none'){
+        show.style.display = 'block';
+        b2.style.display='none';
+        b1.style.display='none';
+        b4.style.display='none';
+        b5.style.display='none';
+        b6.style.display='none';
+    }
+    else{
+        show.style.display = 'none';
+    }
+}
+
+function display4(){
+    let show = document.getElementById('b4');
+    if(show.style.display == 'none'){
+        show.style.display = 'block';
+        b2.style.display='none';
+        b3.style.display='none';
+        b1.style.display='none';
+        b5.style.display='none';
+        b6.style.display='none';
+    }
+    else{
+        show.style.display = 'none';
+    }
+}
+
+function display5(){
+    let show = document.getElementById('b5');
+    if(show.style.display == 'none'){
+        show.style.display = 'block';
+        b2.style.display='none';
+        b3.style.display='none';
+        b4.style.display='none';
+        b1.style.display='none';
+        b6.style.display='none';
+    }
+    else{
+        show.style.display = 'none';
+    }
+}
+
+function display6(){
+    let show = document.getElementById('b6');
+    if(show.style.display == 'none'){
+        show.style.display = 'block';
+        b2.style.display='none';
+        b3.style.display='none';
+        b4.style.display='none';
+        b5.style.display='none';
+        b1.style.display='none';
+    }
+    else{
+        show.style.display = 'none';
+    }
+}
+
